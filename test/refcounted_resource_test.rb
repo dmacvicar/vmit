a083d4d192fe1c929016b16155c6cf6dbfbdd047
Duncan Mac-Vicar P <dmacvicar@suse.de>
2014-02-04 13:17:07 +0100

    Useless assignment to variable

--- test/refcounted_resource_test.rb
+++ test/refcounted_resource_test.rb
@@ -24,8 +24,7 @@ require 'vmit/refcounted_resource'
 
 class RefcountedResource_test < Test::Unit::TestCase
   def test_creation
-    r1 = Vmit::RefcountedResource.new('test1')
-
-    r1 = Vmit::RefcountedResource.new('test1')
+    Vmit::RefcountedResource.new('test1')
+    Vmit::RefcountedResource.new('test1')
   end
 end
